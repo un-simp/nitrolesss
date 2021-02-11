5b1559e7d8f10af12632ed5c73aeb8d48092fd32
Paras Khanchandani <paras_theloveable@hotmail.com>
2021-02-11 15:21:53 +0530

    Changed some CSS, added Paginator logic (needs implementation)

--- contents/main.js
+++ contents/main.js
@@ -14,6 +14,23 @@ var loadPage = {
             console.error(err);
         }
     },
+    paginator: function(items, page, per_page) {
+        var page = page || 1,
+            per_page = per_page || 10,
+            offset = (page - 1) * per_page,
+            paginatedItems = items.slice(offset).slice(0, per_page),
+            total_pages = Math.ceil(items.length / per_page);
+
+        return {
+            page: page,
+            per_page: per_page,
+            pre_page: page - 1 ? page - 1 : null,
+            next_page: (total_pages > page) ? page + 1 : null,
+            total: items.length,
+            total_pages: total_pages,
+            data: paginatedItems
+        }
+    },
     displayEmotes: function(emotes){
         const htmlString = emotes.map((emotes) => {
                 return `<div id="${emotes.name}" class="emoteContainer">
@@ -70,6 +87,6 @@ var loadPage = {
         })
         this.searchBar.addEventListener("focusout", (e) => {
             loadPage.search(e);
-        })
+        });
     }
 }
