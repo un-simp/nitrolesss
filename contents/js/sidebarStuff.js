87087226bb4f3069e0c7b2ab0a9ae5cfd88d6fe0
un_simp(PBOI) <uncraftablecreeper@gmail.com>
2021-06-21 18:54:05 +0100

    Update sidebarStuff.js

--- contents/js/sidebarStuff.js
+++ contents/js/sidebarStuff.js
@@ -55,6 +55,12 @@ var sidebarStuff = {
                         customRepos.repoEmotes = [];
                         loadPage.loadEmotes();
                         break;
+                    case "customReposButton":
+                        e.target.classList.add("active");
+                        searchbar.classList.add("hide");
+                        addRepoButton.classList.remove("hidden");
+                        loadPage.loadCustomRepos();
+                        break;
                     case "aboutButton":
                         e.target.classList.add("active");
                         searchbar.classList.add("hide");
